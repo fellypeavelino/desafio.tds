b84f136bd373df73846cb13ca5716946a63fb9c3
fellypeavelino <fellypeavelino@hotmail.com>
2025-03-12 19:32:39 -0300

    primeiro comite

---
+++ src/main/java/com/desafio/tds/Desafio/TDS/DesafioTdsApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.tds.Desafio.TDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/tds/Desafio/TDS/DesafioTdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.tds.Desafio.TDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
